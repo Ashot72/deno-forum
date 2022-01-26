b052c1f339a18d1645be49f5f01642942d35a3e0
Ashot <ashota@gmail.com>
2022-01-26 12:45:51 +0400

    changing dash to underscore

--- backend/middlewares/middleware.ts
+++ backend/middlewares/middleware.ts
@@ -1,4 +1,4 @@
 export * from "./error.middleware.ts";
 export * from "./request_validator.middleware.ts";
-export * from "./jwt-auth.middleware.ts";
+export * from "./jwt_auth.middleware.ts";
 export * from "./user_guard.middleware.ts";
